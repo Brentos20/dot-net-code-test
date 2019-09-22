53a82f6fa28c517b6caf781e0ef5f0cc51fbff26
Brent Rendall <Brent.Rendall@hotmail.com>
2019-09-22 19:58:56 +0100

    Completed Task 3 (bar D)

--- Vuture.CodingTest/Vuture.CodingTest.Tests/Vulture.Coding.Test.Tests/ProgramTests.cs
+++ Vuture.CodingTest/Vuture.CodingTest.Tests/Vulture.Coding.Test.Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Vuture.CodingTest;
 
@@ -8,6 +9,10 @@ namespace Vulture.Coding.Test.Tests
     public class ProgramTests
     {
         Program program = new Program();
+        List<string> mockList = new List<string>(new string[] {"cat", "dog", "large"});
+        string mockText = "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.";
+        string mockResult = "";
+
         [TestMethod]
         public void TestCountOccursSuccess()
         {
@@ -23,13 +28,37 @@ namespace Vulture.Coding.Test.Tests
         [TestMethod]
         public void TestIsPalinSuccess()
         {
-            Assert.IsTrue(program.isPalindrome("hannah"));
+            Assert.IsTrue(program.IsPalindrome("hannah"));
         }
 
         [TestMethod]
         public void TestIsPallinFail()
         {
-            Assert.IsFalse(program.isPalindrome("abcgicba"));
+            Assert.IsFalse(program.IsPalindrome("abcgicba"));
+        }
+
+        [TestMethod]
+        public void TestCensoredWordsOccurSuccess()
+        {
+            Assert.AreEqual("cat: 1, dog: 2, large: 1, total: 4", program.CensoredWordsOccurSum(mockList, mockText));
+        }
+
+        [TestMethod]
+        public void TestCensorTextSuccess()
+        {
+            mockText = "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse.";
+            mockList = new List<string>( new string[] { "meow","woof"});
+            mockResult = "I have a cat named M$$w and a dog name W$$f. I love the dog a lot. He is larger than a small horse.";
+            Assert.AreEqual(mockResult, program.CensorText(mockList, mockText));
+        }
+
+        [TestMethod]
+        public void TestCensorPalindrome()
+        {
+            mockText = "Anna went to vote in the election to fulfil her civic duty";
+            mockResult = "A$$a went to vote in the election to fulfil her c$$$c duty";
+
+            Assert.AreEqual(mockResult, program.CensorPalindrones(mockText));
         }
     }
 }
--- Vuture.CodingTest/Vuture.CodingTest/Program.cs
+++ Vuture.CodingTest/Vuture.CodingTest/Program.cs
@@ -14,21 +14,92 @@ namespace Vuture.CodingTest
             int count = 0;
             foreach (char ch in text)
             {
-                if (ch.Equals(lett)) count++;
+                if (char.ToLower(ch).Equals(char.ToLower(lett))) count++;
             }
             return count;
         }
 
-        public bool isPalindrome(string text)
+        public int CountOccurs(string sString, string text)
+        {
+            int count = 0;
+            int subLength = sString.Length;
+            for(int i = 0; i<text.Length-subLength; i ++)
+            {
+                if (text.Substring(i, subLength).Equals(sString, StringComparison.OrdinalIgnoreCase)) count++;
+            }
+            return count;
+        }
+
+        public bool IsPalindrome(string text)
         {
             char[] textRev = text.ToCharArray();
             Array.Reverse(textRev);
 
-            for(int i = 0; i < text.Length; i++)
+            return string.Equals(text, new string(textRev), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public string CensoredWordsOccurSum(List<string> CenWords, string text)
+        {
+            StringBuilder ToReturn = new StringBuilder();
+            int total = 0;
+
+            foreach(string cenWord in CenWords)
             {
-                if (!textRev[i].Equals(text[i])) return false;
+                int thisTotal = CountOccurs(cenWord, text);
+                total += thisTotal;
+                ToReturn.Append(string.Format("{0}: {1}, ", cenWord, thisTotal));
             }
-            return true;
+
+            return ToReturn.Append("total: " + total).ToString();
+        }
+
+        public string CensorText(List<string> CenWords, string text)
+        {
+            string cenText = text;
+
+            foreach (string CenWord in CenWords)
+            {
+                
+                int occurs = CountOccurs(CenWord, text);
+                
+                while (occurs>0)
+                {
+                    int start = text.IndexOf(CenWord, StringComparison.OrdinalIgnoreCase);
+                    string wordToCensor = text.Substring(start, CenWord.Length);
+                    cenText = cenText.Replace(wordToCensor, Censored(wordToCensor));
+                    occurs--;
+
+                }
+                
+            }
+            return cenText;
+        }
+
+        private string Censored(string cenWord)
+        {
+            int length = cenWord.Length;
+            char fLett = cenWord[0];
+            char lLett = cenWord[length - 1];
+            StringBuilder Censored = new StringBuilder().Append(fLett);
+            for (int i = 0; i < length - 2; i++) Censored.Append("$");
+
+            return Censored.Append(lLett).ToString();
+        }
+
+        public string CensorPalindrones(string text)
+        {
+            List<string> TextAsList = text.Split(' ').ToList<string>();
+            StringBuilder censoredText = new StringBuilder();
+
+            foreach(string Word in TextAsList)
+            {
+                if (IsPalindrome(Word)) censoredText.Append(Censored(Word));
+                else censoredText.Append(Word);
+
+                censoredText.Append(" ");
+            }
+            censoredText.Remove(censoredText.Length - 1, 1);
+            return censoredText.ToString();
         }
 
         static void Main(string[] args)
